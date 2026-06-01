e60aeaae0deffb49035d5cec5409e29124af2a04
Teacher <2197615920qq@qq.com>
2026-06-02 03:45:31 +0800

    gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  // base 仅在GitHub Pages时需要，本地开发注释掉
-  // base: '/salary-calc/',
+  base: '/salary-calc/',
   plugins: [
     react(),
     tailwindcss(),
